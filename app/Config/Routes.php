69e4c55c62f453a2346ca41841f4b85738360627
rafly <raflyramadhani105@gmail.com>
2026-04-23 00:49:24 +0700

    tambah session

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/keranjang', 'TransaksiController::index');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
