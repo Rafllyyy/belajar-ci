b2ed175dd70f15cf64b73361895a1b32d27ec60d
Rafllyyy <raflyramadhani105@gmail.com>
2026-06-22 00:54:54 +0700

    menambahkan api data produk dan transaksi

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -33,6 +33,12 @@
                 <span>Produk</span>
             </a>
         </li><!-- End Produk Nav --> 
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+                <i class="bi bi-person"></i>
+                <span>History</span>
+            </a>
+        </li><!-- End History Nav -->
         <?php
         }
         ?>
